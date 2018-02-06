be4bc0e5de5f66fe7b8624b68ec3661fcda38fb0
Shreya Prabhu <shreyaprabhu96@gmail.com>
2018-02-06 17:12:07 +0530

    Add script for thinning given character

---
+++ preprocessing/thinning.py
@@ -0,0 +1,24 @@
+from skimage.morphology import skeletonize_3d
+from skimage import color
+import cv2
+from PIL import Image
+from skimage.util import invert
+from scipy.misc import toimage
+
+# Read and store image
+image = cv2.imread(r'images\khu.png')
+# The given image should be in Grayscale or Binary format
+# skeletonize_3d assumes White as foreground and black as background.
+# Hence we invert the image (This can be removed based on what form of input images we decide to provide at later stage)
+image = color.rgb2gray(invert(image))
+# skeletonize_3d is mainly used for 3D images but can be used for 2D also. 
+# Advantage - Removes spurs and provides better output
+skeleton = skeletonize_3d(image)
+
+# Saving output image 
+im = toimage(skeleton)
+im.save("images\skeletonised.png")
+
+# show image
+cv2.imshow("skeleton", skeleton)
+cv2.waitKey(0)
