bc56f512c4ddab514c6675edcea93fe8457b8143
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-02-17 20:24:00 +0530

    preprocessing: add script to denoise images

---
+++ preprocessing/denoise.py
@@ -0,0 +1,17 @@
+import os
+import sys	
+import cv2
+
+# TODO: loop over all images
+image = sys.argv[1]
+filename = filename = os.path.splitext(image)[0]
+
+# open image using openCV
+img = cv2.imread(image)
+
+# denoise image
+dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+
+# save image
+cv2.imwrite(filename + '_denoise.png', dst)
+
