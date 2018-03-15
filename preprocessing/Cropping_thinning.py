c5f1a9478890b90167e057b77972a4b59bde1fdb
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-03-15 21:03:40 +0530

    preprocessing: clean and refactor Cropping_thinning.py
    * Follow a proper structure for code sections
    * Use built-in funtions for copying the entire directory tree

--- preprocessing/Cropping_thinning.py
+++ preprocessing/Cropping_thinning.py
@@ -12,14 +12,6 @@ from joblib import Parallel, delayed 	# Lightweight pipelining: using Python fun
 import cv2 	# Open CV is uesd image processing
 from scipy.misc import toimage # It provides routines for numerical integration and optimization.
 
-start_time = time.time()	# Used to start time record
-count=0 					# Count for no.of files
-rootdir = sys.argv[1]		# Take aregument from command line 
-ori=os.getcwd()				# Save current directory for looping stage
-folder = "Preprocessed_" + sys.argv[1]	# Creating new folder to save the preprocessed images 
-newfolder = os.path.join(os.getcwd(),folder)	# Creating the path to the new folder
-if not os.path.exists(newfolder):	# Check if directory already exists
-	os.makedirs(newfolder)	# Making new directory
 # This function is used to crop and thin the image file
 # We pass th 'im' file as a parameter
 def ct(im):
@@ -42,6 +34,16 @@ def ct(im):
 	# Saving output image
 	image = toimage(skeleton) 	# Takes a numpy array and returns a PIL image
 	image.save(im)
+
+start_time = time.time()	# Used to start time record
+count=0 					# Count for no.of files
+rootdir = sys.argv[1]		# Take aregument from command line
+ori=os.getcwd()				# Save current directory for looping stage
+folder = "Preprocessed Images"     # Creating new folder to save the preprocessed images
+newfolder = os.path.join(os.getcwd(),folder)	# Creating the path to the new folder
+if not os.path.exists(newfolder):	# Check if directory already exists
+		os.makedirs(newfolder)	# Making new directory
+
 # The loop below is used to iterate through each folder and subfolder
 # It generats a tupel root,dirs,files
 # Root is the current directory
@@ -54,25 +56,14 @@ for root,dirs,files in os.walk(rootdir,topdown=False):
 		os.chdir(os.path.abspath(dir3))	# We are moving into the folder specified by dir3 using the absolute path
 		# Now since we are inside the requied directory we check for images
 		# The loop below is used to iterate through each file that ends with '.png' later we can add '.PNG'
-		subfolder = os.path.join(newfolder,name)	# We	make a path with'name' to the new folder		
-		if not os.path.exists(subfolder): # Check if subfolder already exists
-			os.makedirs(subfolder) # We make the folder
+		dst = os.path.join(newfolder,name)	# We make a path with'name' to the new folder
+		if os.path.exists(dst):
+			shutil.rmtree(dst)
 		# The cropped images have been saved in the same folder as original images
 		# We will now move them to the new folder in there specified subfolder
 		src = os.getcwd() 	# source directory is the path of the  subfolder we are currently in
-		dst = subfolder 	# The destination is the new subfolder created 
-		files1 = os.listdir(src)	# creates a list of all files in the source directory
-		# We make a loop which iterates through each files in the directory
-		for f1 in files1: 
-			src_file = os.path.join(src, f1)	# Path to source file
-			dst_file = os.path.join(dst, f1)	# # Path to destination file
-			if (f1.startswith("img")):				# IMPORTENT! make sure the image file starts with 'img' 
-				if os.path.exists(dst_file):		# prior to processing with this program
-					os.remove(dst_file)			# Basic overwiter function
-					shutil.copy(src_file, dst_file)
-				else:
-					shutil.copy(src_file, dst_file)	# This makes usre we only move those files and overwrite if it existes 
-		os.chdir(subfolder)		# Now we move into the subfolder 
+		shutil.copytree(src, dst, symlinks=False, ignore=None)
+		os.chdir(dst)		# Now we move into the subfolder
 		flist=glob.glob('*.png')	# Creats a list of files that end with '.png' later we can add '.PNG'
 		count=count+len(flist)	# Count number of files being processed
 		# The function below is used to parallelize the image processing
@@ -81,6 +72,7 @@ for root,dirs,files in os.walk(rootdir,topdown=False):
 		Parallel(n_jobs=-1)(delayed(ct)(n) for n in flist)
 		print("done with ",name)	# Once done with 
 		os.chdir(ori)	# At the end of the loop we move back to original directory so that looping can start again
+
 end_time = time.time()	# Used to stop time record
 seconds=end_time - start_time
 m, s = divmod(seconds, 60)
