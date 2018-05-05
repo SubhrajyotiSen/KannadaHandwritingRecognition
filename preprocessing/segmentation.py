65f7d3baf00271fd900851603a64e2d91e3de740
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-05-05 12:43:10 +0530

    Revert "segmentation: one script to rule them all with augmentation"
    This reverts commit bae63da5e9cc1fe62c795b6fa9cf31553a428271.

--- preprocessing/segmentation.py
+++ preprocessing/segmentation.py
@@ -6,7 +6,6 @@ import cv2
 import segment_sentence
 import segment_word
 import segment_character
-import augment
 
 file = sys.argv[1] # Get file name from parameter
 fileName = filename = os.path.splitext(file)[0] # Take name of file without extension
@@ -18,22 +17,19 @@ if not os.path.exists(directory):
 
 # Read the image as numpy array
 image = cv2.imread(file)
-s=0
+
 # Get sentences as separate images
 sentences = segment_sentence.segment_sentence(image)
 
 for i in range(0,len(sentences)):
 	# Get words as separate images
 	words = segment_word.segment_word(sentences[i])
-	s+=1
-	w=0
+
 	for j in range(0,len(words)):
 		# Get characters as separate images
 		characters = segment_character.segment_character(words[j])
-		w+=1
-		c=0
+
 		for k in range(0,len(characters)):
-			c+=1
 			""" 
 				Generate image name based on position in original image
 			 	Format is LL-WW-CC where
@@ -41,11 +37,11 @@ for i in range(0,len(sentences)):
 			 		WW is word number in LL
 			 		CC is character number in WW
 			 """
-			imageName = str(s).zfill(2) + '-' + str(w).zfill(2) + '-' + str(c).zfill(2) + '.png'
+			imageName = str(i).zfill(2) + '-' + str(j).zfill(2) + '-' + str(k).zfill(2) + '.png'
 
 			# save image
 			cv2.imwrite(os.path.join(directory, imageName), characters[k])
 
-augment.augment(directory)
+
 
 
