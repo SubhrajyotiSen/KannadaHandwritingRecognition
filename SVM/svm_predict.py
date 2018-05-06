60e5e8ee95f0428e898091667160f4babf218997
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2018-05-06 11:00:22 +0530

    svm: modularise svm scripts

--- SVM/svm_predict.py
+++ SVM/svm_predict.py
@@ -1,14 +1,14 @@
 """
     TO EXECUTE SCRIPT
 
-    python svm_predict.py testfolder N file
+    svm_predict(testfolder, N, file)
 
     testfolder - The folder containing the test images
     N - Number of classes present
     file - File to load the trained model. The file type should be ".dat"
 
     example - 
-                python svm_predict.py test 10 modelsave.dat
+                svm_predict("chars", 10, "digits.dat")
 
 """
 
@@ -20,7 +20,7 @@ import os
 import sys
 
 SZ = 52
-CLASS_N = int(sys.argv[2])
+CLASS_N = 0
 matchcount = 0
 mismatchcount = 0
 
@@ -66,33 +66,55 @@ def mismatch():
     mismatchcount = mismatchcount + 1
     return
 
-def testmyModel():
-	hog = get_hog()
-	modelsave = sys.argv[3]
-	model = cv2.ml.SVM_load(modelsave)
-	test_path = sys.argv[1]
-	for folder in os.listdir(test_path):
-		print("\n##############  ", folder, "  ##############")
-		hog_descriptors = []
-		for file in os.listdir(os.path.join(test_path,folder)):
-			img_predict = cv2.imread(os.path.join(test_path, folder, file),0)
-			hog_descriptors.append(hog.compute(deskew(img_predict)))	
-		hog_descriptors = np.squeeze(hog_descriptors)
-		prediction = svmPredict(model,hog_descriptors)
-		x = len(prediction)	
-		for i in range(x):
-			if(int(prediction[i])==int(folder)):
-				#print("Match",": ", "---", "prediction", ": ", prediction[i])
-				match()
-			else:
-				mismatch()
-				print("MissMatch",": ", file, "---", "prediction", ": ", prediction[i])
-	print("===================================================")
-	print("number of mismatches:",mismatchcount)
-	print("number of matches:",matchcount)
-	totalfiles=mismatchcount+matchcount
-	acc=(matchcount/totalfiles)*100
-	print("total number of letters:",totalfiles)
-	print("accuracy:",'%.2f'%acc,"%")
+def testmyModel(test_path, no_of_classes, modelsave):
+    global CLASS_N
+    CLASS_N = no_of_classes
+    hog = get_hog()
+    model = cv2.ml.SVM_load(modelsave)
+    for folder in os.listdir(test_path):
+        print("\n##############  ", folder, "  ##############")
+        hog_descriptors = []
+        for file in os.listdir(os.path.join(test_path,folder)):
+            img_predict = cv2.imread(os.path.join(test_path, folder, file),0)
+            hog_descriptors.append(hog.compute(deskew(img_predict)))    
+        hog_descriptors = np.squeeze(hog_descriptors)
+        prediction = svmPredict(model,hog_descriptors)
+        x = len(prediction) 
+        for i in range(x):
+            if(int(prediction[i])==int(folder)):
+                print("Match",": ", "---", "prediction", ": ", prediction[i])
+                match()
+            else:
+                mismatch()
+                print("MissMatch",": ", file, "---", "prediction", ": ", prediction[i])
+    print("===================================================")
+    print("number of mismatches:",mismatchcount)
+    print("number of matches:",matchcount)
+    totalfiles=mismatchcount+matchcount
+    acc=(matchcount/totalfiles)*100
+    print("total number of letters:",totalfiles)
+    print("accuracy:",'%.2f'%acc,"%")
 
-testmyModel()
+"""
+    Provide the path of the folder that has all the images which have to be 
+    classified and recognised
+
+    Function returns a dictionary of image names and its corresponding class value detected
+"""
+def svm_predict(test_path, no_of_classes, modelsave):
+    global CLASS_N
+    CLASS_N = no_of_classes
+    hog = get_hog()
+    model = cv2.ml.SVM_load(modelsave)
+    hog_descriptors = []
+    files = []
+    for file in os.listdir(test_path):
+        files.append(file)
+        img_predict = cv2.imread(os.path.join(test_path, file),0)
+        hog_descriptors.append(hog.compute(deskew(img_predict)))    
+    hog_descriptors = np.squeeze(hog_descriptors)
+    prediction = svmPredict(model,hog_descriptors)
+    mypredictions = {}
+    for i in range(0,len(files)):
+        mypredictions[files[i]] = prediction[i]
+    return(mypredictions)
--- SVM/svm_train.py
+++ SVM/svm_train.py
@@ -1,14 +1,14 @@
 """
-	TO EXECUTE SCRIPT
+    TO EXECUTE SCRIPT
 
-	python svm_train.py train_image N file
+    svm_train("train_image", N, file)
 
-	train_image - The training image having train dataset
-	N - Number of classes present 
-	file - File to store the model trained. The file type should be ".dat"
+    train_image - The training image having train dataset
+    N - Number of classes present 
+    file - File to store the model trained. The file type should be ".dat"
 
-	example - 
-				python svm_train.py train.jpg 10 modelsave.dat
+    example - 
+                svm_train("test.jpg", 10, "digits.dat")
 
 """
 
@@ -24,52 +24,52 @@ import sys
 # Size of each image
 SZ = 52
 # No of classes present
-CLASS_N = int(sys.argv[2]) 
+CLASS_N = 0
 c_accuracy_set = []
 
 # Split the images into cells based on SZ which is 52*52
 def split2d(img, cell_size, flatten=True):
-	h, w = img.shape[:2]
-	sx, sy = cell_size
-	cells = [np.hsplit(row, w//sx) for row in np.vsplit(img, h//sy)]
-	cells = np.array(cells)
-	if flatten:
-		cells = cells.reshape(-1, sy, sx)
-	return cells
+    h, w = img.shape[:2]
+    sx, sy = cell_size
+    cells = [np.hsplit(row, w//sx) for row in np.vsplit(img, h//sy)]
+    cells = np.array(cells)
+    if flatten:
+        cells = cells.reshape(-1, sy, sx)
+    return cells
 
 # Load the image data, split and label them
 def load_digits(fn):
-	digits_img = cv2.imread(fn, 0)
-	digits = split2d(digits_img, (SZ, SZ))
-	labels = np.repeat(np.arange(CLASS_N), len(digits)/CLASS_N)
-	return digits, labels
+    digits_img = cv2.imread(fn, 0)
+    digits = split2d(digits_img, (SZ, SZ))
+    labels = np.repeat(np.arange(CLASS_N), len(digits)/CLASS_N)
+    return digits, labels
 
 # deskew image
 def deskew(img):
-	m = cv2.moments(img)
-	if abs(m['mu02']) < 1e-2:
-		return img.copy()
-	skew = m['mu11']/m['mu02']
-	M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
-	img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
-	return img
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+    return img
 
 # SVM initialisation
 # C=12.5, gamma=0.001
 def svmInit(C, gamma):
-	model = cv2.ml.SVM_create()
-	model.setGamma(gamma)
-	model.setC(C)
-	model.setKernel(cv2.ml.SVM_RBF)
-	model.setType(cv2.ml.SVM_C_SVC)
-	return model
+    model = cv2.ml.SVM_create()
+    model.setGamma(gamma)
+    model.setC(C)
+    model.setKernel(cv2.ml.SVM_RBF)
+    model.setType(cv2.ml.SVM_C_SVC)
+    return model
 
 def svmTrain(model, samples, responses):
-	model.train(samples, cv2.ml.ROW_SAMPLE, responses)
-	return model
+    model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+    return model
 
 def svmPredict(model, samples):
-	return model.predict(samples)[1].ravel()
+    return model.predict(samples)[1].ravel()
 
 # Evaluate trained model. Print Accuracy obtained during validation 
 def svmEvaluate(model, digits, samples, labels):
@@ -99,43 +99,37 @@ def get_hog() :
     affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
 
   
-if __name__ == '__main__':
-
-    print('Loading ... ')
-    # Load image data.
-    imageName = sys.argv[1]
-    # Get the name of the file to store model. The file type required is ".dat"
-    modelsave = sys.argv[3]
-
+def svm_train(imageName, no_of_classes, modelsave):
+    
+    global CLASS_N
+    CLASS_N = no_of_classes
     digits, labels = load_digits(imageName)
 
-    print('Shuffle data ... ')
     # Shuffle data
     rand = np.random.RandomState(10)
     shuffle = rand.permutation(len(digits))
     digits, labels = digits[shuffle], labels[shuffle]
     
-    print('Deskew images ... ')
+    # Deskew images
     digits_deskewed = list(map(deskew, digits))
-    
-    print('Defining HoG parameters ...')
+
     # HoG feature descriptor
     hog = get_hog()
 
-    print('Calculating HoG descriptor for every image ... ')
+    # Calculate HOG descriptors for all the images
     hog_descriptors = []
     for img in digits_deskewed:
         hog_descriptors.append(hog.compute(img))
     hog_descriptors = np.squeeze(hog_descriptors)
     print(hog_descriptors.shape)
 
-    print('Spliting data into training (90%) and test set (10%)... ')
+    # Spliting data into training (90%) and test set (10%)
     train_n=int(0.9*len(hog_descriptors))
     digits_train, digits_test = np.split(digits_deskewed, [train_n])
     hog_descriptors_train, hog_descriptors_test = np.split(hog_descriptors, [train_n])
     labels_train, labels_test = np.split(labels, [train_n])
 
-    print('Training SVM model ...')
+    
     # Used to generate SVM Learning curve graph
     """for m in range(1,15,3):
         model = svmInit(m,0.001)
@@ -143,11 +137,12 @@ if __name__ == '__main__':
         print('Evaluating model ... ')
         vis = svmEvaluate(model, digits_test, hog_descriptors_test, labels_test, m)
         print(m)"""
-    
+
+    # Train SVM Model
     model = svmInit(12.5,0.001)
     svmTrain(model, hog_descriptors_train, labels_train)
-    print('Evaluating model ... ')
+    # Evaluate model
     vis = svmEvaluate(model, digits_test, hog_descriptors_test, labels_test)
-    print('Saving SVM model ...')
+    # Save model
     model.save(modelsave)
     cv2.destroyAllWindows()
