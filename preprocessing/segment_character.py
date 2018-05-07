03ba059378ad4f1e686599fbfee29259d836aaf6
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2018-05-08 01:09:17 +0530

    add code to segment ottaksharas and map them to their parent character

--- preprocessing/segment_character.py
+++ preprocessing/segment_character.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import sys
 import ntpath
+from matplotlib import pyplot as plt
 
 """ To sort the contours in 4 ways
 		left-to-right
@@ -34,15 +35,17 @@ def sort_contours(cnts, method="left-to-right"):
 	return (cnts, boundingBoxes)
 
 def segment_character(image):
-
+	image = cv2.imread(image, 0)
+	row, col = image.shape
+	print(row,col)
 	ret,thresh = cv2.threshold(image,127,255,cv2.THRESH_BINARY_INV)
 	ret,thresh2 = cv2.threshold(image,127,255,cv2.THRESH_BINARY)
 	im2,ctrs, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 	(ctrs, boundingBoxes) = sort_contours(ctrs, method="left-to-right")
 
-	characters = []
-
+	characters = {}
+	ottaksharas = {}
 	# For each contour, find the bounding rectangle and draw it
 	for i,cnt in enumerate(ctrs):
 		x,y,w,h = cv2.boundingRect(cnt)
@@ -54,8 +57,47 @@ def segment_character(image):
 		# Find the segmented character and store
 		roi = thresh2[y:y+h, x:x+w]
 
-		characters.append(roi)
+		# Analyse the contour bounding box x,y,h,w values to get better understanding
+		print(y,y+h,x,x+w)
+		
+		""" 
+			Ottakshara is always present at bottom when compared with rest of the characters in the word.
 
-	return characters
+			Hence when we draw contours, the contours of ottaksharas begin at a height lesser than 
+			50% of the total height of the image. 
+			(We are making an assumption of 50% here which works well most of the time)
+
+			Based on above condition, store the roi accordingly into character and ottakshara dictionaries
+
+			character dictionary
+				char_contour_id: roi
+
+			ottakshara dictionary
+				parent_char_contour_id: roi
+			where parent_char_contour_id maps to char_contour_id of character dictionary
+
+			This works well in case only one ottakshara is present for each character.
+
+			TODO: 
+					Make method more efficient and make it possible to map multiple ottaksharas belonging to one parent character
+					How? 
+					Map ottakshara to character based on x values now.
+					compare ottakshara(x) and every character(x+h) values and map to closest character
+
+		"""
+		if(y>(row/2)):
+			cv2.rectangle(image,(x,y),( x + w, y + h ),(0,0,255),1)
+			ottaksharas[i-1] = roi
+		else:
+			characters[i] = roi
+	cv2.imshow("cont", image)
+	cv2.waitKey(0)
+	#plotimage(image)
+	ottkeys = ottaksharas.keys()
+	print("ottkeys: ",ottkeys)
+	charkeys = characters.keys()
+	print("charkeys: ",charkeys)
+	return characters, ottaksharas
 		
 
+segment_character("Segmented_index/word12.png")
