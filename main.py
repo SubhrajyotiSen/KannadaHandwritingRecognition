0c2014ce3e27588a0405f46e2e22a0268e0aa32e
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-05-11 01:02:27 +0530

    integration: Use batch recognition
    * loading the models separately every time for a new image caused a huge delay. Instead just open them once and do all the recognition
    * thanks to @stevejerold1 for pointing this out

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from preprocessing.segmentation import segment
 from preprocessing.augment import augment
 
 from CNN.recognize_character import recognize
-from CNN.ottakshara_dict import ottakshara_mapping 
 
 from Unicode.seqgen import sequenceGen
 from Unicode.printdoc import unicode_to_kn
 
 def analyze(image):
-
+	
 	# directory name to store the segmented image
 	dir = 'Segmented_' + os.path.splitext(ntpath.basename(image))[0]
 
@@ -22,20 +21,8 @@ def analyze(image):
 	# augment each of the segmented images
 	augment(dir)
 
-	# sort the list of image names alphabetically
-	flist = os.listdir(dir)
-	flist.sort()
-
-	predictions = {}
-
-	# iterate through each image and predict its class
-	for file in flist:
-		# if image is an ottakshara
-		if '-1' in file:
-			predictions[file] = ottakshara_mapping[recognize(os.path.abspath(os.path.join(dir, file)),'ottakshara')]
-		#if image in a regular character
-		else:
-			predictions[file] = recognize(os.path.abspath(os.path.join(dir, file)),'other')
+	# recognize all images in the directory
+	predictions = recognize(os.path.join(os.getcwd(),dir))
 
 	# generate the Unicode sequence based on predictions
 	sequence = sequenceGen(predictions)
