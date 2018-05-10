6012b01de6eb5b2d9011dcedca7da7d97a2405e1
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-05-10 11:50:07 +0530

    CNN: modularize recognize_character.py

--- CNN/recognize_character.py
+++ CNN/recognize_character.py
@@ -6,19 +6,12 @@ import sys
 from keras.models import load_model
 from keras import backend as K
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-tf.logging.set_verbosity(tf.logging.ERROR)
-model = load_model('cnn_model.h5')
-
-def get_image_size():
-	img = cv2.imread(imageName, 0)
-	return img.shape
-
-imageName = sys.argv[1]
-image_x, image_y = get_image_size()
+def init():
+	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+	tf.logging.set_verbosity(tf.logging.ERROR)
 
 def keras_process_image(img):
-	print(img.shape)
+	image_x, image_y = img.shape
 	img = np.reshape(img, (1, image_x, image_y, 1))
 	return img
 
@@ -26,14 +19,15 @@ def keras_predict(model, image):
 	processed = keras_process_image(image)
 	pred_probab = list(model.predict(processed)[0])
 	sorted_probab = sorted(pred_probab, reverse=True)
-	for x in sorted_probab[0:5]:
-		print(x, pred_probab.index(x))
+	return pred_probab.index(sorted_probab[0])
 
-def recognize():
+def recognize(imageName,flag):
+	init()
+	if flag == 'ottakshara':
+		model = load_model('CNN/cnn_model_ottak.h5')
+	else:
+		model = load_model('CNN/cnn_model.h5')
 	image = cv2.imread(imageName, cv2.IMREAD_GRAYSCALE)
-	keras_predict(model, image)
-			
-
-#keras_predict(model, np.zeros((28, 28), dtype=np.uint8))		
-recognize()
-K.clear_session();
+	prediction = keras_predict(model, image)
+	K.clear_session();
+	return prediction
